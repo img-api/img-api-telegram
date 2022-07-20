6a1b95378519cc565f5a7abee2d9f3fcd8ff10e5
Sergio A. Martinez <sergio@engineer.blue>
2022-07-20 06:25:52 +0000

    [Example] Added an example
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

---
+++ imgapi_config_example.py
@@ -0,0 +1,21 @@
+telegram_config = {
+    'token': 'ONLY FROM BOT FATHER',
+    'system': 'IMG-API.COM',
+    'bot_name': 'img-api-bot-name'
+}
+
+user_config = {
+    'username': "my.test.user",
+    'password': "",
+    'first_name': "",
+    'last_name': "",
+    'is_public': True,
+    'is_media_public': True,
+    'email': "",
+}
+
+telethon_config = {
+    'API_ID': '<https://my.telegram.org/apps>',
+    'API_HASH': '<https://my.telegram.org/apps>',
+    'PHONE_NUMBER': '+ PHONE NUMBER'
+}
--- update.sh
+++ update.sh
@@ -4,6 +4,12 @@ echo "UPDATING IMG-API"
 
 cd "${BASH_SOURCE%/*}"
 
+if [ ! -d ".venv" ]
+then
+   echo "INSTALLING VENV "
+   python3 -m venv .venv
+fi
+
 . .venv/bin/activate
 
 pip3 install -r requirements.txt --upgrade
